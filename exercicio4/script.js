e1eabddda92ec448071d516f74867144f30a3289
Dionei Silva <dionei.silva@gmail.com>
2022-12-01 14:25:26 -0300

    AtividadePratica 05Estruturas Condicionais

---
+++ exercicio4/script.js
@@ -0,0 +1,7 @@
+/* 4. Numa promoção exclusiva para o Dia da Mulher, uma loja quer dar
+descontos para todos, mas especialmente para mulheres. Faça um
+programa que leia nome, sexo e o valor das compras do cliente e
+calcule o preço com desconto. Sabendo que:
+a. Homens ganham 5% de desconto
+
+b. Mulheres ganham 13% de desconto */
---
+++ exercicio6/script.js
@@ -0,0 +1,7 @@
+/* 6. Faça um programa que leia a largura e o comprimento de um
+terreno retangular, calculando e mostrando a sua área em m2. O
+programa também deve mostrar a classificação desse terreno, de
+acordo com a lista abaixo:
+Abaixo de 100m2 = TERRENO POPULAR
+Entre 100m2 e 500m2 = TERRENO MASTER
+Acima de 500m2 = TERRENO VIP */
