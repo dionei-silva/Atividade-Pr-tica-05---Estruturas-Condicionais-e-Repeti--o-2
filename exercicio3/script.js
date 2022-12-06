877b4fb108249b63fc550ea32c0b17414f8005cf
Dionei Silva <dionei.silva@gmail.com>
2022-12-06 06:30:26 -0300

    Exercicio3-ok

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -5,3 +5,12 @@ Digite o primeiro Valor: 3
 Digite o último Valor: 10
 Digite o incremento: 2
 Contagem: 3 5 7 9 Acabou! */
+
+let numero1 = 3;
+let numero2 = 10;
+let numero3 = 2;
+
+for (let i = numero1; i <= numero2; i += numero3) {
+    document.write(`${i} `);
+}
+document.write("Acabou!");
