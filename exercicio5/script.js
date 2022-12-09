818bb4e9fd2ff878952702a00020d69c1447b49d
Dionei Silva <dionei.silva@gmail.com>
2022-12-09 13:19:23 -0300

    exercicio5-ok

--- exercicio5/script.js
+++ exercicio5/script.js
@@ -2,3 +2,13 @@
 deseja percorrer em Km. Calcule o preço da passagem, cobrando
 R$0.50 por Km para viagens até 200Km e R$0.45 para viagens mais
 longas. */
+
+function atividade5() {
+    let distância = Number(prompt("digite distancia: "));
+    if (distância < 200) {
+        document.write(`Valor passagem é ${distância * 0.5}`);
+    } else {
+        distância < 200;
+        document.write(`Valor passagem é ${distância * 0.45}`);
+    }
+}
