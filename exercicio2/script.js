6b33b8fe7232c545324ea8a5260b8be0ab42185a
Dionei Silva <dionei.silva@gmail.com>
2022-12-09 12:46:03 -0300

    exercicio2 ok

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -3,3 +3,39 @@ funcionários. No final, mostre o total de salários pagos aos homens e
 o total pago às mulheres. O programa vai perguntar ao usuário se
 ele quer continuar ou não sempre que ler os dados de um
 funcionário. */
+
+function atividade2() {
+    let continuar;
+    while (continuar !== 1) {
+        let salario1 = 0;
+        let salario2 = 0;
+
+        let sexo = prompt("Informe o sexo").toLowerCase();
+        if (sexo == "h" || sexo == "homem") {
+            sexo = 1;
+        } else if (sexo == "m" || sexo == "mulher") {
+            sexo = 2;
+        } else {
+            alert("sexo invalido");
+            break;
+        }
+        switch (sexo) {
+            case "1":
+                salario1 += "informar salario";
+                continuar = Number(
+                    prompt("Digite 0 para continuar ou 1 para parar:")
+                );
+                break;
+            default:
+                salario2 += Number(prompt("Informe seu salario:"));
+                continuar = Number(
+                    prompt("Digite 0 para continuar ou 1 para parar:")
+                );
+                break;
+        }
+        if (salario1 > 0) {
+            document.write(<p>Total pago aos homens: ${salario1}</p>);
+            document.write(<p>Total pago as Mulheres: ${salario2}</p>);
+        }
+    }
+}
