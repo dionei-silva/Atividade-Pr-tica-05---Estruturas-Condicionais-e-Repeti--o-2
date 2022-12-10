3b9c623e179d4f96a85aa475bba5a6d3c0faa6b1
Dionei Silva <dionei.silva@gmail.com>
2022-12-10 13:59:51 -0300

    exercicio6 - ok

--- exercicio6/script.js
+++ exercicio6/script.js
@@ -5,3 +5,24 @@ acordo com a lista abaixo:
 Abaixo de 100m2 = TERRENO POPULAR
 Entre 100m2 e 500m2 = TERRENO MASTER
 Acima de 500m2 = TERRENO VIP */
+
+function exercicio6() {
+    let comprimento = Number(prompt("digite o comprimento"));
+    if (!comprimento) {
+        document.write("comprimento invalido");
+        return;
+    }
+    let largura = Number(prompt("digite a largura"));
+    if (!largura) {
+        document.write("largura invalida invalido");
+        return;
+    }
+    let area = comprimento * largura;
+    if (area < 100) {
+        document.write("TERRENO POPULAR");
+    } else if (area > 500) {
+        document.write("TERRENO MASTER");
+    } else {
+        document.write("TERRENO VIP");
+    }
+}
